992729536b58a2fa187b00bcb6212a20d1328627
Kaan Kani İnce <kkince@newmind.ai>
2024-12-02 17:39:01 +0300

    odev4 completed

---
+++ Clean-Blog-Patika-Odev4/app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const ejs = require("ejs");
+const path = require("path");
+const app = express();
+const Blog = require("./models/Blog");
+
+//connectDB
+mongoose.connect("mongodb://localhost/blogs");
+
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//MIDDLEWARES
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//ROUTES
+app.get("/", async (req, res) => {
+  const blogs = await Blog.find({});
+  res.render("index", {
+    blogs: blogs,
+  });
+});
+
+app.get("/blogs/:id", async (req, res) => {
+  // console.log(req.params.id);
+
+  // res.render("about");
+  const post = await Blog.findById(req.params.id);
+  res.render("post", {
+    post,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+
+app.post("/blogs", async (req, res) => {
+  await Blog.create(req.body);
+  res.redirect("/");
+});
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
